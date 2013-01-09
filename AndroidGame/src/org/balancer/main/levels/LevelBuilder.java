502ca70f2dbdd000b322ed9eb7b4a38939d0d6dd
Feax <feax@mail.ru>
2013-01-09 14:27:29 +0900

    Added new crossplatform project. Starting refactoring

---
+++ AndroidGame/src/org/balancer/main/levels/LevelBuilder.java
@@ -0,0 +1,38 @@
+package org.balancer.main.levels;
+
+import org.balancer.main.AndroidGame;
+
+public class LevelBuilder extends AbstractScreen{
+	public void create(AndroidGame game) {
+		super.create(game);
+		trace("LevelBuilder");
+	}
+	
+	public void render() {
+		setColor(1,0,0);
+	}
+	
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+}
